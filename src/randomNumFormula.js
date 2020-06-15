7ac559460eea5e2a06235a1be93e463aca5a4250
Ingo <ingo-o@yandex.ru>
2020-06-16 01:45:31 +0300

    fixed the formula

--- src/randomNumFormula.js
+++ src/randomNumFormula.js
@@ -1,2 +1,2 @@
 // Генерация случайного числа в заданном диопазоне
-export default (min, max) => Math.ceil(Math.random() * (max - min + 1));
+export default (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
